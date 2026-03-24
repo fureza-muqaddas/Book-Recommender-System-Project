a2fb21fa2ee5f22c99544ec703230ee0d550b1a7
Fureza Muqaddas <furezamuqaddas@gmail.com>
2026-03-24 15:23:27 -0400

    updated project

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from src.ds_project.pipeliness.training_pipeline import TrainingPipeline
 obj = TrainingPipeline()
 obj.start_data_ingestion()
 
+
--- src/ds_project/pipeliness/training_pipeline.py
+++ src/ds_project/pipeliness/training_pipeline.py
@@ -1,6 +1,7 @@
 from constant import CONFIG_FILE_PATH
 from src.ds_project.components.data_ingestion import DataIngestion
 from src.ds_project.config.configuration import AppConfiguration
+from src.ds_project.components.data_validation import DataValidation
 
 
 
@@ -9,6 +10,7 @@ class TrainingPipeline:
     def __init__(self):
         self.app_config = AppConfiguration(config_file_path=CONFIG_FILE_PATH)
         self.data_ingestion = DataIngestion(app_config=self.app_config)
+        self.data_validation = DataValidation(app_config=self.app_config)
 
     def start_data_ingestion(self):
         """
@@ -16,3 +18,4 @@ class TrainingPipeline:
         :return: none
         """
         self.data_ingestion.initiate_data_ingestion()
+        self.data_validation.initiate_data_validation()
